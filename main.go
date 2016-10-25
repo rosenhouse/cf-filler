cbf79d669a79977a09acb95733ffc147fc2abf4d
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-10-25 13:33:12 -0700

    Output YAML, generate random password

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 const (
@@ -25,6 +30,24 @@ func (o OutputData) AddSystemComponent(name string, cfgFlags int) {
 	}
 	if cfgFlags&CfgWithHTTPSURL != 0 {
 		o[fmt.Sprintf("%s_url", name)] = fmt.Sprintf("https://%s", uri)
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func generatePassword() string {
+	bytes := make([]byte, 16)
+	if _, err := rand.Read(bytes); err != nil {
+		panic("unable to read rand bytes: " + err.Error())
+	}
+	return strings.Trim(base64.RawURLEncoding.EncodeToString(bytes), "-_")
+}
+
+func (o OutputData) GeneratePasswords(keynames ...string) {
+	for _, name := range keynames {
+		o[name] = generatePassword()
 	}
 }
 
@@ -55,7 +78,15 @@ func mainWithError() error {
 	o["blobstore_private_url"] = "https://blobstore.service.cf.internal:4443"
 	o["metron_agent_deployment_name"] = fmt.Sprintf("%s-cf", envName)
 
-	return json.NewEncoder(os.Stdout).Encode(o)
+	o.GeneratePasswords("blobstore_admin_users_password")
+
+	outBytes, err := yaml.Marshal(o)
+	if err != nil {
+		return fmt.Errorf("marshaling output as yaml: %s", err)
+	}
+	os.Stdout.Write(outBytes)
+
+	return nil
 }
 
 func main() {
