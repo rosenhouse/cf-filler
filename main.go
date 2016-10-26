c6bf16bc159526e5342ff500260d6af3ee1ddc87
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-10-25 21:36:21 -0700

    Cert generation works

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/square/certstrap/pkix"
+
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -122,6 +124,42 @@ func mainWithError() error {
 	o["nats_user"] = "nats"
 	o["router_status_user"] = "router-status"
 
+	o.GenerateCerts(
+		&CA{
+			VarName_CA: "etcd_ca_cert",
+			CommonName: "etcdCA",
+		},
+		&CertKeyPair{
+			VarName_Cert: "etcd_server_cert",
+			VarName_Key:  "etcd_server_key",
+			CommonName:   "etcd.service.cf.internal",
+			Domains: []string{
+				"*.etcd.service.cf.internal",
+				"etcd.service.cf.internal",
+			},
+		},
+		&CertKeyPair{
+			VarName_Cert: "etcd_client_cert",
+			VarName_Key:  "etcd_client_key",
+			CommonName:   "clientName",
+		},
+	)
+	o.GenerateCerts(
+		&CA{
+			VarName_CA: "etcd_peer_ca_cert",
+			CommonName: "peerCA",
+		},
+		&CertKeyPair{
+			VarName_Cert: "etcd_peer_cert",
+			VarName_Key:  "etcd_peer_key",
+			CommonName:   "etcd.service.cf.internal",
+			Domains: []string{
+				"*.etcd.service.cf.internal",
+				"etcd.service.cf.internal",
+			},
+		},
+	)
+
 	outBytes, err := yaml.Marshal(o)
 	if err != nil {
 		return fmt.Errorf("marshaling output as yaml: %s", err)
@@ -137,3 +175,154 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+type CA struct {
+	VarName_CA string
+	CommonName string
+
+	key  *pkix.Key
+	cert *pkix.Certificate
+}
+
+const KeyBits = 2048
+const CAExpiryYears = 10
+const HostCertExpiryYears = 2
+
+func (ca *CA) Init() error {
+	var err error
+	ca.key, err = pkix.CreateRSAKey(KeyBits)
+	if err != nil {
+		return fmt.Errorf("create ca key: %s", err)
+	}
+
+	ca.cert, err = pkix.CreateCertificateAuthority(ca.key, "", CAExpiryYears,
+		"", "", "", "", ca.CommonName)
+	if err != nil {
+		return fmt.Errorf("create ca cert: %s", err)
+	}
+
+	return nil
+}
+
+func (ca *CA) CACertAsString() (string, error) {
+	return asString(ca.cert)
+}
+
+func (ca *CA) InitCertKeyPair(certKeyPair *CertKeyPair) error {
+	var err error
+	certKeyPair.key, err = pkix.CreateRSAKey(KeyBits)
+	if err != nil {
+		return fmt.Errorf("create host key: %s", err)
+	}
+	csr, err := pkix.CreateCertificateSigningRequest(certKeyPair.key, "", nil,
+		certKeyPair.Domains, "", "", "", "", certKeyPair.CommonName)
+	if err != nil {
+		return fmt.Errorf("create host csr: %s", err)
+	}
+
+	certKeyPair.cert, err = pkix.CreateCertificateHost(ca.cert, ca.key, csr, HostCertExpiryYears)
+	if err != nil {
+		return fmt.Errorf("sign host csr: %s", err)
+	}
+
+	return nil
+}
+
+type CertKeyPair struct {
+	VarName_Cert string
+	VarName_Key  string
+	CommonName   string
+	Domains      []string
+
+	key  *pkix.Key
+	cert *pkix.Certificate
+}
+
+type exportable interface {
+	Export() ([]byte, error)
+}
+
+func asString(e exportable) (string, error) {
+	pemBytes, err := e.Export()
+	if err != nil {
+		return "", fmt.Errorf("export pem: %s", err)
+	}
+
+	return string(pemBytes), nil
+}
+
+func (kp *CertKeyPair) PrivateKeyAsString() (string, error) {
+	pemBytes, err := kp.key.ExportPrivate()
+	if err != nil {
+		return "", fmt.Errorf("export private key: %s", err)
+	}
+
+	return string(pemBytes), nil
+}
+
+func (kp *CertKeyPair) CertAsString() (string, error) {
+	return asString(kp.cert)
+}
+
+func (o OutputData) GenerateCerts(ca *CA, certKeyPairs ...*CertKeyPair) {
+	err := ca.Init()
+	if err != nil {
+		panic(err)
+	}
+	o[ca.VarName_CA], err = ca.CACertAsString()
+	if err != nil {
+		panic(err)
+	}
+	for _, certKeyPair := range certKeyPairs {
+		err = ca.InitCertKeyPair(certKeyPair)
+		if err != nil {
+			panic(err)
+		}
+		o[certKeyPair.VarName_Cert], err = certKeyPair.CertAsString()
+		if err != nil {
+			panic(err)
+		}
+		o[certKeyPair.VarName_Key], err = certKeyPair.PrivateKeyAsString()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+/* TODO: add these
+
+blobstore_tls_ca_cert
+	blobstore_tls_cert
+	blobstore_tls_private_key
+
+consul_agent_ca_cert
+	consul_agent_agent_key
+	consul_agent_cert
+	consul_agent_server_cert
+	consul_agent_server_key
+
+diego_bbs_ca_cert
+	diego_bbs_client_cert
+	diego_bbs_client_key
+	diego_bbs_server_cert
+	diego_bbs_server_key
+
+diego_bbs_sql_db_connection_string
+
+diego_ssh_proxy_host_key
+diego_ssh_proxy_host_key_fingerprint
+
+loggregator_tls_ca_cert
+	doppler_tls_server_cert
+	doppler_tls_server_key
+	metron_metron_agent_tls_client_cert
+	metron_metron_agent_tls_client_key
+
+
+uaa_jwt_signing_key
+uaa_jwt_verification_key
+
+uaa_sslCertificate
+uaa_sslPrivateKey
+
+*/
