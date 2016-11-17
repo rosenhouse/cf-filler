4bea598407c617fcdd021eeec4261a0ace601dd5
Gabe Rosenhouse <rosenhouse@gmail.com>
2016-11-16 21:15:23 -0800

    Simplify cert generation code

--- creds/certs.go
+++ creds/certs.go
@@ -32,66 +32,40 @@ func NewCA(commonName string) (*CA, error) {
 		return nil, fmt.Errorf("create ca cert: %s", err)
 	}
 
-	ca.CertPEM, err = asString(ca.cert)
+	caCertPEMBytes, err := ca.cert.Export()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("export pem: %s", err)
 	}
 
+	ca.CertPEM = string(caCertPEMBytes)
 	return ca, nil
 }
 
-type certKeyPair struct {
-	CommonName string
-	Domains    []string
-
-	key  *pkix.Key
-	cert *pkix.Certificate
-}
-
 func (ca *CA) NewCertKeyPair(commonName string, domains []string) (string, string, error) {
-	ckp := &certKeyPair{
-		CommonName: commonName,
-		Domains:    domains,
-	}
-	var err error
-
-	ckp.key, err = pkix.CreateRSAKey(KeyBits)
+	key, err := pkix.CreateRSAKey(KeyBits)
 	if err != nil {
 		return "", "", fmt.Errorf("create host key: %s", err)
 	}
-	csr, err := pkix.CreateCertificateSigningRequest(ckp.key, "", nil,
-		ckp.Domains, "", "", "", "", ckp.CommonName)
+	csr, err := pkix.CreateCertificateSigningRequest(key, "", nil,
+		domains, "", "", "", "", commonName)
 	if err != nil {
 		return "", "", fmt.Errorf("create host csr: %s", err)
 	}
 
-	ckp.cert, err = pkix.CreateCertificateHost(ca.cert, ca.key, csr, HostCertExpiryYears)
+	cert, err := pkix.CreateCertificateHost(ca.cert, ca.key, csr, HostCertExpiryYears)
 	if err != nil {
 		return "", "", fmt.Errorf("sign host csr: %s", err)
 	}
 
-	privateBytes, err := ckp.key.ExportPrivate()
+	privateKeyPEMBytes, err := key.ExportPrivate()
 	if err != nil {
 		return "", "", fmt.Errorf("export private key: %s", err)
 	}
 
-	cert, err := asString(ckp.cert)
+	certPEMBytes, err := cert.Export()
 	if err != nil {
 		return "", "", err
 	}
 
-	return string(privateBytes), cert, nil
-}
-
-type exportable interface {
-	Export() ([]byte, error)
-}
-
-func asString(e exportable) (string, error) {
-	pemBytes, err := e.Export()
-	if err != nil {
-		return "", fmt.Errorf("export pem: %s", err)
-	}
-
-	return string(pemBytes), nil
+	return string(privateKeyPEMBytes), string(certPEMBytes), nil
 }
