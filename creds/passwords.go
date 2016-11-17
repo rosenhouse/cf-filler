a10233ade612cc7e3eee1b273220406c1da88b6a
Gabe Rosenhouse <rosenhouse@gmail.com>
2016-11-16 21:42:04 -0800

    Improve password generation
    - Password length is 22 characters, with 62-character symbol set, for an
      approximate entropy of log2(62^22) = 130 bits
    
    - Remove non-alphanumeric characters so that password is easier to
    select in a text editor or terminal

--- creds/passwords.go
+++ creds/passwords.go
@@ -6,12 +6,21 @@ import (
 	"strings"
 )
 
-const PasswordLength = 16
+// Generate passwords of this length
+const PasswordLength = 22
 
+// NewPassword returns a cryptographically secure pseudorandom string
+// suitable for use as a password, shared secret, etc.
+//
+// The returned string will match the regex [a-zA-Z0-9]*
 func NewPassword() string {
 	bytes := make([]byte, PasswordLength)
 	if _, err := rand.Read(bytes); err != nil {
 		panic("unable to read rand bytes: " + err.Error())
 	}
-	return strings.Trim(base64.RawURLEncoding.EncodeToString(bytes), "-_")
+	return cut(cut(base64.RawURLEncoding.EncodeToString(bytes), "-"), "_")
+}
+
+func cut(orig, toCut string) string {
+	return strings.Replace(orig, toCut, "", -1)
 }
